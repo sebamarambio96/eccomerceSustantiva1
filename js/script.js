b001bbf6626cebf392552639750097afc10d9363
Sebastian Marambio <s.marambiosandoval@gmail.com>
2022-12-27 16:10:56 -0300

    vuevlo a probar con JSON

--- js/script.js
+++ js/script.js
@@ -1,300 +1,67 @@
-const libros = {
-    "categorias":
-    {
-        "mangas": [
-            {
-                "id": 1,
-                "nombre": "Naruto tomo 1",
-                "autor": "Masashi Kishimoto",
-                "precio": 8100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/narutoTomo1.webp"
-            },
-            {
-                "id": 2,
-                "nombre": "Naruto tomo 2",
-                "autor": "Masashi Kishimoto",
-                "precio": 8200,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/narutoTomo2.jfif"
-            },
-            {
-                "id": 3,
-                "nombre": "Naruto tomo 3",
-                "autor": "Masashi Kishimoto",
-                "precio": 8300,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/onePieceTomo3.webp"
-            },
-            {
-                "id": 4,
-                "nombre": "Naruto tomo 4",
-                "autor": "Masashi Kishimoto",
-                "precio": 8100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/onePieceTomo4.jpg"
-            },
-            {
-                "id": 5,
-                "nombre": "One Piece Español Tomo 1",
-                "autor": "Eiichirō Oda",
-                "precio": 9100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/onePieceTomo1.jpg"
-            },
-            {
-                "id": 6,
-                "nombre": "One Piece Español Tomo 2 East Blue",
-                "autor": "Eiichirō Oda",
-                "precio": 9200,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/imgproductoHC11.webp"
-            },
-            {
-                "id": 7,
-                "nombre": "One Piece Español Tomo 3",
-                "autor": "Eiichirō Oda",
-                "precio": 9300,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/onePieceTomo3.webp"
-            },
-            {
-                "id": 8,
-                "nombre": "One Piece Español Tomo 4",
-                "autor": "Eiichirō Oda",
-                "precio": 9400,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Mangas/onePieceTomo4.jpg"
-            }
+fetch("../JSON/productos.json")
+    .then((resp) => resp.json())
+    .then(data => {
 
-        ],
-        "horror": [
-            {
-                "id": 10,
-                "nombre": "La llamada de Cthulu",
-                "autor": "H.P Lovecraft",
-                "precio": 11100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/llamadaCthulhu.jpg"
-            },
-            {
-                "id": 11,
-                "nombre": "Dagon",
-                "autor": "H.P Lovecraft",
-                "precio": 11200,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/dagon.jpeg"
-            },
-            {
-                "id": 12,
-                "nombre": "Necronomicon",
-                "autor": "H.P Lovecraft",
-                "precio": 11300,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/necronomicon.jpg"
-            },
-            {
-                "id": 13,
-                "nombre": "Los Pilares de Cthulu",
-                "autor": "H.P Lovecraft",
-                "precio": 11400,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/pilaresCthulhu.jfif"
-            },
-            {
-                "id": 14,
-                "nombre": "La Maldición de Sarnath",
-                "autor": "H.P Lovecraft",
-                "precio": 11500,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/maldicionSarnath.jpg"
-            },
-            {
-                "id": 15,
-                "nombre": "El caso de Charles Dexter Ward",
-                "autor": "H.P Lovecraft",
-                "precio": 11600,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/casoCharlesDexter.jpg"
-            },
-            {
-                "id": 16,
-                "nombre": "La casa en el confín de la Tierra",
-                "autor": "Richard Corben - Simon Revelstroke",
-                "precio": 12100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/casaConfinesTierra.JPG"
-            },
-            {
-                "id": 17,
-                "nombre": "Los que acechan en el abismo",
-                "autor": "Brian Lumley",
-                "precio": 13100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/habitantesAbismo.jpg"
-            },
-            {
-                "id": 18,
-                "nombre": "Night Shift",
-                "autor": "Stephen King",
-                "precio": 14100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/nightShift.jpg"
-            },
-            {
-                "id": 19,
-                "nombre": "IT",
-                "autor": "Stephen King",
-                "precio": 14200,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Horror/it.jfif"
-            }
-        ],
-        "juvenil": [
-            {
-                "id": 21,
-                "nombre": "Harry Potter y la Piedra Filosofal",
-                "autor": "J.K Rowling",
-                "precio": 15100,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryPiedraFilosofal.webp"
-            },
-            {
-                "id": 22,
-                "nombre": "Harry Potter y la Camara de los Secretos",
-                "autor": "J.K Rowling",
-                "precio": 15200,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryCamaraSecreta.jpg"
-            },
-            {
-                "id": 23,
-                "nombre": "Harry Potter y Prisionero de Azkaban",
-                "autor": "J.K Rowling",
-                "precio": 15400,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryPrisioneroAzkaban.jpg"
-            },
-            {
-                "id": 24,
-                "nombre": "Harry Potter y el Caliz de Fuego",
-                "autor": "J.K Rowling",
-                "precio": 15500,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryCalizFuego.webp"
-            },
-            {
-                "id": 25,
-                "nombre": "Harry Potter y la Orden del Fenix",
-                "autor": "J.K Rowling",
-                "precio": 15600,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryOrdenFenix.jpg"
-            },
-            {
-                "id": 26,
-                "nombre": "Harry Potter y el Principe Mestizo",
-                "autor": "J.K Rowling",
-                "precio": 15600,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryPrincipeMestizo.webp"
-            },
-            {
-                "id": 27,
-                "nombre": "Harry Potter y las Reliquias de la Muerte",
-                "autor": "J.K Rowling",
-                "precio": 15700,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/harryReliquiasMuerte.webp"
-            },
-            {
-                "id": 28,
-                "nombre": "Coraline",
-                "autor": "Neil Gaiman",
-                "precio": 15800,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/coraline.jpg"
-            },           
-            {
-                "id": 29,
-                "nombre": "Buscando a Alaska",
-                "autor": "J.K Rowling",
-                "precio": 15900,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/buscandoAlaska.jpg"
-            },
-            {
-                "id": 20,
-                "nombre": "Animales Fántasticos",
-                "autor": "J.K Rowling",
-                "precio": 15000,
-                "cantidad": 0,
-                "imagen": "../multimedia/Productos/Juvenil/animalesFantasticos1.jpg"
-            }
-        ]
-    }
+        const productos = data.categorias
+        console.log(productos)
 
-}
+        const containerHorror = document.getElementById('containerHorror')
+        const fragment = document.createDocumentFragment()
+        const template = document.getElementById('templateProducto').content;
+        console.log(template);
 
-const productos = libros.categorias
-console.log(productos)
+        productos.horror.forEach(producto => {
+            let imgProducto = template.getElementById('imgProducto')
+            let autorProducto = template.getElementById('autorProducto')
+            let tituloProducto = template.getElementById('nombreProducto')
+            let precioProducto = template.getElementById('precioProducto')
+            imgProducto.src = producto.imagen
+            imgProducto.alt = producto.nombre
+            autorProducto.textContent = producto.autor
+            tituloProducto.textContent = producto.nombre
+            precioProducto.textContent = `$ ${producto.precio}`
+            const clone = template.cloneNode(true)
 
-const containerHorror = document.getElementById('containerHorror')
-const fragment = document.createDocumentFragment()
-const template = document.getElementById('templateProducto').content;
-console.log(template);
+            fragment.appendChild(clone)
+        })
+        containerHorror.appendChild(fragment)
 
-productos.horror.forEach(producto => {
-    let imgProducto = template.getElementById('imgProducto')
-    let autorProducto = template.getElementById('autorProducto')
-    let tituloProducto = template.getElementById('nombreProducto')
-    let precioProducto = template.getElementById('precioProducto')
-    imgProducto.src = producto.imagen
-    console.log(imgProducto.src)
-    imgProducto.alt = producto.nombre
-    autorProducto.textContent = producto.autor
-    tituloProducto.textContent = producto.nombre
-    precioProducto.textContent = `$ ${producto.precio}`
-    const clone = template.cloneNode(true)
+        const containerMangas = document.getElementById('containerMangas')
+        const fragment2 = document.createDocumentFragment()
 
-    fragment.appendChild(clone)
-})
-containerHorror.appendChild(fragment)
+        productos.mangas.forEach(producto => {
+            let imgProducto = template.getElementById('imgProducto')
+            let autorProducto = template.getElementById('autorProducto')
+            let tituloProducto = template.getElementById('nombreProducto')
+            let precioProducto = template.getElementById('precioProducto')
+            imgProducto.src = producto.imagen
+            imgProducto.alt = producto.nombre
+            autorProducto.textContent = producto.autor
+            tituloProducto.textContent = producto.nombre
+            precioProducto.textContent = `$ ${producto.precio}`
+            const clone = template.cloneNode(true)
 
-const containerMangas = document.getElementById('containerMangas')
-const fragment2 = document.createDocumentFragment()
+            fragment2.appendChild(clone)
+        })
+        containerMangas.appendChild(fragment2)
 
-productos.mangas.forEach(producto => {
-    let imgProducto = template.getElementById('imgProducto')
-    let autorProducto = template.getElementById('autorProducto')
-    let tituloProducto = template.getElementById('nombreProducto')
-    let precioProducto = template.getElementById('precioProducto')
-    imgProducto.src = producto.imagen
-    imgProducto.alt = producto.nombre
-    autorProducto.textContent = producto.autor
-    tituloProducto.textContent = producto.nombre
-    precioProducto.textContent = `$ ${producto.precio}`
-    const clone = template.cloneNode(true)
+        const containerJuvenil = document.getElementById('containerJuvenil')
+        const fragment3 = document.createDocumentFragment()
 
-    fragment2.appendChild(clone)
-})
-containerMangas.appendChild(fragment2)
+        productos.juvenil.forEach(producto => {
+            let imgProducto = template.getElementById('imgProducto')
+            let autorProducto = template.getElementById('autorProducto')
+            let tituloProducto = template.getElementById('nombreProducto')
+            let precioProducto = template.getElementById('precioProducto')
+            imgProducto.src = producto.imagen
+            imgProducto.alt = producto.nombre
+            autorProducto.textContent = producto.autor
+            tituloProducto.textContent = producto.nombre
+            precioProducto.textContent = `$ ${producto.precio}`
+            const clone = template.cloneNode(true)
 
-const containerJuvenil = document.getElementById('containerJuvenil')
-const fragment3 = document.createDocumentFragment()
-
-productos.juvenil.forEach(producto => {
-    let imgProducto = template.getElementById('imgProducto')
-    let autorProducto = template.getElementById('autorProducto')
-    let tituloProducto = template.getElementById('nombreProducto')
-    let precioProducto = template.getElementById('precioProducto')
-    imgProducto.src = producto.imagen
-    imgProducto.alt = producto.nombre
-    autorProducto.textContent = producto.autor
-    tituloProducto.textContent = producto.nombre
-    precioProducto.textContent = `$ ${producto.precio}`
-    const clone = template.cloneNode(true)
-
-    fragment3.appendChild(clone)
-})
-containerJuvenil.appendChild(fragment3)
+            fragment3.appendChild(clone)
+        })
+        containerJuvenil.appendChild(fragment3)
+    })
+.catch ()
